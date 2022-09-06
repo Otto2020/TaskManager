0703415073db17a369c10863e4296fadd04e85fe
ottoj <ottoj@OTTO-PC>
2022-09-06 10:59:15 -0600

    Add project files.

---
+++ TaskManager/Task Manager.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace TaskManager
+{
+    partial class TaskManager
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtNumPro = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column5,
+            this.Column3,
+            this.Column4,
+            this.Column6});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(714, 409);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(12, 415);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(85, 23);
+            this.btnKill.TabIndex = 1;
+            this.btnKill.Text = "Kill Process";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(103, 415);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtNumPro
+            // 
+            this.txtNumPro.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNumPro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNumPro.Location = new System.Drawing.Point(576, 415);
+            this.txtNumPro.Name = "txtNumPro";
+            this.txtNumPro.Size = new System.Drawing.Size(100, 23);
+            this.txtNumPro.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Process";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "PID";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.Width = 50;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Priority";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column5.Width = 50;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Physical Memory";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.Width = 130;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Virtual Memory";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.Width = 120;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "CPU";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // TaskManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.txtNumPro);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "TaskManager";
+            this.Text = "Task Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtNumPro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ TaskManager/Task Manager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TaskManager
+{
+    public partial class TaskManager : Form
+    {
+        public TaskManager()
+        {
+            InitializeComponent();
+            ListAllProcesses();
+            timer1.Enabled = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        public void ListAllProcesses()
+        {
+            dataGridView1.Rows.Clear();
+            Process[] AllProcess = Process.GetProcesses();
+            foreach(Process p1 in AllProcess)
+            {
+                try
+                {
+                    dataGridView1.Rows.Add(p1.ProcessName, p1.Id,p1.BasePriority, p1.WorkingSet64, p1.VirtualMemorySize64, p1.SessionId);
+                }
+                catch (Exception)
+                {
+                }
+
+            }
+            txtNumPro.Text = "Processes: " + dataGridView1.Rows.Count.ToString();
+        }
+
+        public void UpdateTable()
+        {
+            ListAllProcesses();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                Process p = Process.GetProcessById(Int32.Parse(dataGridView1.SelectedRows[0].Cells[1].Value.ToString()));
+                p.Kill();
+            } catch (Exception)
+            {
+                MessageBox.Show("No se ha seleccionado ningún proceso para detener.");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateTable();
+        }
+    }
+}
